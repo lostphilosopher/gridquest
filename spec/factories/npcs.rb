41f3b8d08b59f017309ca848c13d3e64c6fce044
lostphilosopher <wandersen02@gmail.com>
2016-02-13 10:45:55 -0600

    Working rake tests

--- spec/factories/npcs.rb
+++ spec/factories/npcs.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :npc do
     after(:build) do |npc|
-      FactoryGirl.create(:stat, npc: npc) unless npc.stat
+      #FactoryGirl.create(:stat, npc: npc) unless npc.stat
     end
   end
 end
