7a38a0a029d84493c232b016ac245687d1a6036d
lostphilosopher <wandersen02@gmail.com>
2016-02-18 01:58:52 -0600

    Domain: GridQuest.com

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_quenzars_grids_session'
+Rails.application.config.session_store :cookie_store, key: '_grid_quest_session'
